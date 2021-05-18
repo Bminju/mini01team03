f33b744c4690e024989b7c76df0378b96392a8f9
Bminju <minjuuuz@gmail.com>
2021-05-18 15:13:48 +0900

    CostContrller remove unused code

--- src/main/java/mini01team03/cost/controller/CostController.java
+++ src/main/java/mini01team03/cost/controller/CostController.java
@@ -1,20 +1,14 @@
 package mini01team03.cost.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import mini01team03.cost.model.CostVO;
-import mini01team03.cost.model.MarkerVO;
 
 @Controller
 @RequestMapping("cost")
@@ -40,19 +34,16 @@ public class CostController {
 	//안혜지 Msg
 	// 백민주 MSG
 	//허슬기 Msg
-	
-	@ResponseBody
-	@PostMapping("cost/save")
-	public Map costSave(@RequestBody MarkerVO markerVO[]) {
-		for(int i = 0; i < markerVO.length; i++) {
-			System.out.println(markerVO[i].getTitle());
-			System.out.println(markerVO[i].getAddress());
-			System.out.println(markerVO[i].getLa());
-			System.out.println(markerVO[i].getMa());
-		}
-		Map map = new HashMap();
-		map.put("msg", "success");
-		return map;
-	}
+	/*
+	 * @ResponseBody
+	 * 
+	 * @PostMapping("cost/save") public Map costSave(@RequestBody MarkerVO
+	 * markerVO[]) { for(int i = 0; i < markerVO.length; i++) {
+	 * System.out.println(markerVO[i].getTitle());
+	 * System.out.println(markerVO[i].getAddress());
+	 * System.out.println(markerVO[i].getLa());
+	 * System.out.println(markerVO[i].getMa()); } Map map = new HashMap();
+	 * map.put("msg", "success"); return map; }
+	 */
 	
 }
